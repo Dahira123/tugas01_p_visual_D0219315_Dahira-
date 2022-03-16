f2b7efcc4d8f3d7405e91f60c45ff13757ea3c54
Dahira123 <101113787+Dahira123@users.noreply.github.com>
2022-03-16 07:50:30 -0700

    Add files via upload

---
+++ TUGAS01_DAHIRA_D0219315_B.py
@@ -0,0 +1,87 @@
+print("Dahira")
+print("D0219315")
+
+print('=' * 25)
+print("1. Penjumlahan")
+print("2. Pengurangan")
+print("3. Perkalian")
+print("4. Pembagian")
+print('=' * 25)
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = x+y
+elif pil == 2:
+   hasil = x-y
+elif pil == 3:
+   hasil = x*y
+elif pil == 4:
+   hasil = x/y
+
+print("\nHasil : %d" %hasil)
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = x+y
+elif pil == 2:
+   hasil = x-y
+elif pil == 3:
+   hasil = x*y
+elif pil == 4:
+   hasil = x/y
+print("\nHasil : %d" %hasil)
+
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = x+y
+elif pil == 2:
+   hasil = x-y
+elif pil == 3:
+   hasil = x*y
+elif pil == 4:
+   hasil = x/y
+print("\nHasil : %d" %hasil)
+
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = x+y
+elif pil == 2:
+   hasil = x-y
+elif pil == 3:
+   hasil = x*y
+elif pil == 4:
+   hasil = x/y
+print("\nHasil : %d" %hasil)
+
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = x+y
+elif pil == 2:
+   hasil = x-y
+elif pil == 3:
+   hasil = x*y
+elif pil == 4:
+   hasil = x/y
+print("\nHasil : %d" %hasil)
